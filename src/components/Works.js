acb3884b3d413bb39e4ec08b1f9127db42f8a469
sharif-git-dev <sharefjammoo@gmail.com>
2024-12-31 02:32:13 +0300

    works Updated

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,7 +1,5 @@
-// src/components/HeroSection.js
 import React from 'react';
 import './Style.css';
-import { Container, Row, Col, Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 import { useState } from 'react';
 import sorcero from '../assets/Project_img/sorcero.jpg';
@@ -9,106 +7,93 @@ import Adeptia from '../assets/Project_img/Adeptia.jpg';
 import SOLBooks from '../assets/Project_img/Solbooks.jpg';
 import YHH from '../assets/Project_img/YHH.png';
 import Ekhtibary from '../assets/Project_img/Ekhtibary.jpg';
+import {Row , Col} from 'react-bootstrap';
+import { color } from 'framer-motion';
 
 const Services = () => {
   const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 768);
-
   const projects = [
     {
       title: "Sorcero",
       description: "Sorcero transforms scattered medical data into high-quality insights, providing teams with instant access to information that improves and saves patient lives.",
       image: sorcero,
-      link: "/Sorcero", // Link to the Sorcero project page
+      link: "/Sorcero",
     },
     {
       title: "Adeptia",
       description: "Adeptia offers a no-code platform that simplifies data integration and process automation, allowing organizations to connect applications and streamline workflows effectively.",
       image: Adeptia,
-      link: "/Adeptia", // Link to the Adeptia project page
+      link: "/Adeptia",
     },
     {
       title: "SOLBooks",
       description: "SOLBooks is an accounting application designed to help businesses manage their finances efficiently, providing tools for invoicing, expense tracking, and financial reporting.",
       image: SOLBooks,
-      link: "/SOLBooks", // Link to the SOLBooks project page
+      link: "/SOLBooks",
     },
     {
       title: "YH&H Project",
       description: "The YH&H Project is a project management website that streamlines team collaboration, task tracking, and project timelines.",
       image: YHH,
-      link: "/Yh-h", // Add the link for the YH&H project
+      link: "/Yh-h",
     },
     {
       title: "Ekhtibary",
       description: "Ekhtibary is a platform designed for managing educational assessments.",
       image: Ekhtibary,
-      link: "/Ekhtibary", // Link to the Ekhtibary project page
+      link: "/Ekhtibary",
     },
   ];
 
   return (
-    <div>
-      <Row className='full-section align-content-center'>
-        <h1 className='Question2 text-white text-center'>Featured Works</h1>
-        <Col>
-          <Carousel className='m-5 p-5 pe-lg-5- pb-lg-5'>
-            {projects.map((project, index) => (
-              <Carousel.Item key={index}>
-                <div>
-                  {isMobileView ? (
-                    <div>
-                      <div className='text-start'>
-                        <h1 className='Question2 text-white'>{String(index + 1).padStart(2, '0')}.</h1>
-                        <h3 className='Question3 text-white'>{project.title}</h3>
-                      </div>
-                      <div className="image-container">
-                        <img
-                          src={project.image}
-                          alt={project.title}
-                          className="img-fluid"
-                        />
-                        <div className="overlay">
-                          <p>{project.description}</p>
-                          <Link to={project.link}>Learn More</Link> {/* Button to navigate to the project page */}
-                        </div>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className='row'>
-                      <div className='col'></div>
-                      <div className='col text-start '>
-                        <div className=''>
-                          <h1 className='worknum'>{String(index + 1).padStart(2, '0')}.</h1>
-                          <h3 className='Question3 text-white'>{project.title}</h3>
-                          <Link to={project.link}>
-                            <button className='m-1 p-2 pe-3 ps-3 rounded-5'>
-                              View Details
-                            </button>
-                          </Link>
-                        </div>
-                      </div>
-                      <div className='col'>
-                        <div className="image-container">
-                          <img
-                            src={project.image}
-                            alt={project.title}
-                            className="img-fluid"
-                          />
-                          <div className="overlay p-5">
-                            <p>{project.description}</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='col'></div>
-
-                    </div>
-                  )}
-                </div>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </Col>
-      </Row>
+    <div className="container pt-5 pb-5">
+      <h1 className="Question3 text-white text-start">Featured Works</h1>
+      <div className="row row-cols-1 row-cols-lg-2">
+        {projects.map((project, index) => (
+          <div className='col'>
+          {isMobileView ? (
+            
+             <div key={index} className=" pb-5">
+              
+             <Link to={project.link} className="widget-content ">
+             <div className="widget-content  ">
+                   <h3 className=" Question3  text-white">{project.title}</h3>
+                   <div className="text-start">
+                     <p className=" text-white">
+                       {project.description}
+                     </p>
+                   </div>
+                 </div>
+             <div className='bg-black position-absolute blackoverlay '>asdsas</div>
+               <div
+                 className="widget-mobile "
+                 style={{ backgroundImage: `url(${project.image})`, color: 'white'}}
+               >
+                
+               </div>
+             </Link>
+           </div>
+          ) : (
+            <div key={index} className="col pt-5 pb-5 pe-5 text-center">
+            <Link to={project.link} className="widget-content">
+                  <h3 className="text-start text-white Question3 ">{project.title}</h3>
+              <div
+                className="widget"
+                style={{ backgroundImage: `url(${project.image})` }}
+              >
+               <div className="widget-overlay">
+                    <p className="widget-description text-white text-center">
+                      {project.description}
+                    </p>
+                  </div>
+              </div>
+             
+            </Link>
+          </div>
+          )}
+        </div>
+        ))}
+      </div>
     </div>
   );
 };
